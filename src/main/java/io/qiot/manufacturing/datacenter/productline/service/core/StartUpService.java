fc174384c085010665e6cb1fb41bc12641887d10
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-19 13:41:11 +0200

    improve GraalVM reflection;Fix log

--- src/main/java/io/qiot/manufacturing/datacenter/productline/service/core/StartUpService.java
+++ src/main/java/io/qiot/manufacturing/datacenter/productline/service/core/StartUpService.java
@@ -5,16 +5,27 @@ import javax.enterprise.event.Event;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import org.jboss.resteasy.plugins.providers.DocumentProvider;
+import org.jboss.resteasy.plugins.providers.FormUrlEncodedProvider;
+import org.jboss.resteasy.plugins.providers.IIOImageProvider;
+import org.jboss.resteasy.plugins.providers.JaxrsFormProvider;
+import org.jboss.resteasy.plugins.providers.SourceProvider;
+import org.jboss.resteasy.plugins.providers.sse.SseEventProvider;
 import org.slf4j.Logger;
 
 import io.qiot.manufacturing.datacenter.productline.domain.event.NewGlobalProductLineEventDTO;
 import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
 /**
  * @author andreabattaglia
  *
  */
 @ApplicationScoped
+@RegisterForReflection(targets = { IIOImageProvider.class,
+        SseEventProvider.class, FormUrlEncodedProvider.class,
+        SourceProvider.class, DocumentProvider.class, JaxrsFormProvider.class })
+
 public class StartUpService {
     @Inject
     Logger LOGGER;
